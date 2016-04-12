c91b50ec04a563e641f5b995c3fadc812b7a893a
Afton Downey <downeydillon@gmail.com>
2016-04-12 16:36:34 -0700

    All tests passing.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,17 +4,34 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-
     get("/", (request, response) -> {
-      HashMap model = new HashMap();
+      HashMap<String, Object> model = new HashMap<>();
       model.put("template", "templates/index.vtl" );
+      model.put("tasks", request.session().attribute("tasks"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    post("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      ArrayList<Task> tasks = request.session().attribute("tasks");
+
+      if (tasks == null) {
+        tasks = new ArrayList<Task>();
+        request.session().attribute("tasks", tasks);
+      }
+
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+      tasks.add(newTask);
+
+      model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
